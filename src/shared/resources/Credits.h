eeac60c4ad3eb5f685c309c40d0328ef1785040b
Luskus_Deus <neatnessplay@gmail.com>
2026-08-23 23:30:58 +0300

    credits: add kira. and medomij to the testers list
    Signed-off-by: Luskus_Deus <neatnessplay@gmail.com>

--- src/shared/resources/Credits.h
+++ src/shared/resources/Credits.h
@@ -20,7 +20,8 @@ inline const QStringList& testers()
     static const QStringList names = { QStringLiteral("kaixxxy"), QStringLiteral("Lozar"),
         QStringLiteral("Mikko_el"), QStringLiteral("HipaaaH!~"), QStringLiteral("Ayami"),
         QStringLiteral("Dgan"), QStringLiteral("KrOl"), QStringLiteral("KAMENOV PLUS"),
-        QStringLiteral("Enum Nektovse") };
+        QStringLiteral("Enum Nektovse"), QStringLiteral("kira."),
+        QStringLiteral("medomij") };
     return names;
 }
 
